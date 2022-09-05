3cea0c5f13237a84c13fb8783ff502c0aa4a8d2f
java-santosh <santosh.paudel@infodevelopers.com.np>
2022-09-05 20:38:27 +0545

    first commit

---
+++ src/main/java/com/example/Dhamal/DhamalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dhamal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhamalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhamalApplication.class, args);
+	}
+
+}
